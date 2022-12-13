2d3e13b1430d11eb01cdb99a8d22719dba011650
Halcomercio <halcomercioutec@gmail.com>
2022-12-13 04:56:14 +0000

    Update

--- frontend/templates/js/getProductos.js
+++ frontend/templates/js/getProductos.js
@@ -11,11 +11,14 @@ function getProductos(){
 
     tableHead.innerHTML = `
         <tr>
-            <th>Nombre</th>
+            <th>Imagen</th>
+            <th>Nombre</th>  
             <th>Precio</th>
             <th>Cantidad</th>
             <th>Categoria</th>
             <th>Descripcion</th>
+            <th>Favoritos</th>
+            <th><th>
         </tr>
     `;
 
@@ -27,24 +30,33 @@ function getProductos(){
         for (let i = 0; i < json.length; i++){
 
             var tr = document.createElement("tr");
+            var tr_imagen = document.createElement("td");
             var tr_nombre = document.createElement("td");
             var tr_precio = document.createElement("td");
             var tr_cantidad = document.createElement("td");
             var tr_categoria = document.createElement("td");
             var tr_descripcion = document.createElement("td");
-
+            var tr_favorito = document.createElement("td");
+            var tr_ver = document.createElement("td");
+           
+            tr_imagen.innerHTML = '<img aria-label="producto" src="css/coco.jpg"/>';
             tr_nombre.innerHTML = json[i].nombre;
             tr_precio.innerHTML = json[i].precio;
             tr_cantidad.innerHTML = json[i].stock;
             tr_categoria.innerHTML = json[i].categoria;
             tr_descripcion.innerHTML = json[i].descripcion;
+            tr_favorito.innerHTML = `<a aria-label="fav" id="agregar-favoritos" class="fa fa-heart" href="#"></a>`;
+            tr_ver.innerHTML = "<a href='#'>Detalles</a>";
 
             // DAR A CADAR TR(COLUMNAS) DEL HTML LOS VALORES DE CADA FILA
+            tr.appendChild(tr_imagen);
             tr.appendChild(tr_nombre);
             tr.appendChild(tr_precio);
             tr.appendChild(tr_cantidad);
             tr.appendChild(tr_categoria);
             tr.appendChild(tr_descripcion);
+            tr.appendChild(tr_favorito);
+            tr.appendChild(tr_ver);
             // PASAR AL BODY DE LA TABLA LOS DATOS 
             tableBody.appendChild(tr);
         }
